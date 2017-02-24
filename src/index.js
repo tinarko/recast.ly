e0eb7e292908880da0a83ffc786d306f7df68116
tinarko <tinarchan@gmail.com>
2017-02-23 20:16:41 -0800

    Add debounce

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // TODO: Render the `App` component to the DOM
 
-ReactDOM.render(<App searchYouTube={window.searchYouTube} API_KEY={window.YOUTUBE_API_KEY}/>, document.getElementById('app'));
+ReactDOM.render(<App searchYouTube={_.debounce(window.searchYouTube, 500)} API_KEY={window.YOUTUBE_API_KEY}/>, document.getElementById('app'));
 
